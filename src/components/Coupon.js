5b2b3bc6cfdd1e2a8d7d3b0558cbf28ee41d1fcb
김영선 <kys@gim-yeongseon-ui-MacBook-Air.local>
2019-08-28 23:07:56 +0900

    20190828

--- src/components/Coupon.js
+++ src/components/Coupon.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
 
 export default props => {
+  const [data, setData] = useState('');
 
+  useEffect(() => {
+    axios.get('http://issuev3apitest.m2i.kr:9999/serviceapi.asmx/ServiceCouponList?CODE=1230&PASS=manbo&DOCCODE=1230000')
+      .then(res =>{
+        setData(res);
+      })
+      .catch(err => console.log(err))
+  }, [])
   return (
-    <h1>Coupon Page</h1>
+    <Fragment>
+      <h1>Coupon Page</h1>
+      <p>{data}</p>
+    </Fragment>
   )
 }
